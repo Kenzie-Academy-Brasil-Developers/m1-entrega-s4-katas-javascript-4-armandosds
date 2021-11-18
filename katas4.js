2ca542dd28abc89cfa16fa098efb5d5d2313dccd
Armando Soares <armandosds@gmail.com>
2021-11-18 12:00:16 -0300

    Primeira função

--- katas4.js
+++ katas4.js
@@ -1,13 +1,121 @@
+const showResults = (imp) => {
+    console.log(imp);
+}
+
+const criaElementos = () => {
+    const div = document.getElementById('container');
+    const h4 = document.createElement('h4');
+    h4.innerText = showResults;
+    h4.appendChild(div);
+}
+
 const gotCitiesCSV = "King's Landing,Braavos,Volantis,Old Valyria,Free Cities,Qarth,Meereen";
 
 const lotrCitiesArray = ["Mordor","Gondor","Rohan","Beleriand","Mirkwood","Dead Marshes","Rhun","Harad"];
 
 const bestThing = "The best thing about a boolean is even if you are wrong you are only off by a bit";
 
-function kata1() {
+const kata1 = () => {
+    let resultado = gotCitiesCSV.split(',');
+    let array = [];
+    for (let i = 0; i < resultado.length; i++) {
+        array.push(resultado[i]);
+    }
+    return array;
+}
+showResults(kata1());
+
+const kata2 = () => {
     // implemente aqui o código
 }
+// kata2();
 
-// kata1();
+const kata3 = () => {
+    // implemente aqui o código
+}
+// kata3();
 
-// daqui em diante é contigo
+const kata4 = () => {
+    // implemente aqui o código
+}
+// kata4();
+
+const kata5 = () => {
+    // implemente aqui o código
+}
+// kata5();
+
+const kata6 = () => {
+    // implemente aqui o código
+}
+// kata6();
+
+const kata7 = () => {
+    // implemente aqui o código
+}
+// kata7();
+
+const kata8 = () => {
+    // implemente aqui o código
+}
+// kata8();
+
+const kata9 = () => {
+    // implemente aqui o código
+}
+// kata8();
+
+const kata10 = () => {
+    // implemente aqui o código
+}
+// kata10();
+
+const kata11 = () => {
+    // implemente aqui o código
+}
+// kata11();
+
+const kata12 = () => {
+    // implemente aqui o código
+}
+// kata12();
+
+const kata13 = () => {
+    // implemente aqui o código
+}
+// kata13();
+
+const kata14 = () => {
+    // implemente aqui o código
+}
+// kata14();
+
+const kata15 = () => {
+    // implemente aqui o código
+}
+// kata15();
+
+const kata16 = () => {
+    // implemente aqui o código
+}
+// kata16();
+
+const kata17 = () => {
+    // implemente aqui o código
+}
+// kata17();
+
+const kata18 = () => {
+    // implemente aqui o código
+}
+// kata18();
+
+const kata19 = () => {
+    // implemente aqui o código
+}
+// kata19();
+
+const kata20 = () => {
+    // implemente aqui o código
+}
+// kata20();
